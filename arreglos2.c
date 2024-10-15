8863aeddae2f78ec0dc094d07c8818b7a7410858
Rodrigo O. <rokamotoresendiz2@gmail.com>
2024-10-14 21:31:52 -0600

    Update arreglos2.c

--- arreglos2.c
+++ arreglos2.c
@@ -3,9 +3,22 @@
 int main(){
     int arr[]= {1, 2, 3, 4, 5};
     //TODO: (4) Declara un apuntador que apunte al primer elemento del arreglo
-
+    int *ptr;
+    ptr = arr;
+    
 
     //TODO: (5) Recorre el arreglo utilizando notación de punteros
+ printf("[ ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d, ", *(ptr + i));
+    }
+    printf("]\n");
 
+    //TODO: HACER UN CICLO QUE IMPRIMA DESDE EL TERCER EN ADELANTE USANDO NOTACION DE PUNTEROS
+    printf("[ ");
+    for (int i = 2; i < 5; i++) {
+        printf("%d, ", *(ptr + i));
+    }
+    printf("]\n");
     return 0;
 }
