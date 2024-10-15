b5ad79c3e3c794bedf2fa8ed493ffa8bf8063c1a
Rodrigo O. <rokamotoresendiz2@gmail.com>
2024-10-14 21:30:08 -0600

    Update arreglos1.c

--- arreglos1.c
+++ arreglos1.c
@@ -12,9 +12,24 @@ int main(){
     str2[5] = 's';
 
     // TODO: (1) Recorre arr mediante un índice usando un ciclo
-
+printf("[ ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf(" ]\n");
     // TODO: (2) Recorre str mediante un índice usando un ciclo
-    
+  printf("[ ");
+    for (int i = 0; i < strlen(str); i++) {
+        printf("%c ", str[i]);
+    }
+    printf("]\n");  
 
     // TODO:(3) Recorre str2 mediante un índice usando un ciclo
+     printf("[ ");
+    for (int i = 0; i < strlen(str2); i++) {
+        printf("%c ", str[i]);
+    }
+    printf("]\n");
+
+    return 0;
 }
