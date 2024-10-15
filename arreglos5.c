72d31011e7b070b099b6e0bb5003267ff1519604
Rodrigo O. <rokamotoresendiz2@gmail.com>
2024-10-14 22:23:37 -0600

    Update arreglos5.c

--- arreglos5.c
+++ arreglos5.c
@@ -3,10 +3,14 @@
 int main() {
     int a = 10, b = 20, c = 30;
     //TODO: (14) Declara un arreglo de tres apuntadores a entero
-    
+    int *a[3];
     
     //TODO: (15) Asigna a cada apuntador la dirección de las variables a, b y c
+    a[0] = 10;
+    b[1] = 20;
+    c[2] = 30;
     //TODO: (16) Mostrar los valores usando los punteros
+    
     
     return 0;
 }
