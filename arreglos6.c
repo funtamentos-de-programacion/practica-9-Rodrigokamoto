d9e63dc5888a6eac8062e89cc88d4e291620fbd4
Okamoto Resendiz Rodrigo Alexis <rokamotoresendiz2@gmail.com>
2024-10-17 10:46:37 -0600

    Solucion de los ejerecicios del 6 al 9

--- arreglos6.c
+++ arreglos6.c
@@ -2,11 +2,22 @@
 
 int main() {
     //TODO: (17) Asigna lo valores del 1 al 9 para rellenar la matriz de 3x3
-    int arr[3][3];
+    int arr[3][3] = {{1,2,3},{4, 5, 6},{7, 8, 9}};
     
     // Acceso usando índices
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
+            printf("%d\t", arr[i][j]);
+
+            //TODO: (18) Usa i y j para acceder alos elementos de la matriz
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%p\n", &arr[i][j]);
+
             //TODO: (18) Usa i y j para acceder alos elementos de la matriz
         }
     }
--- arreglos8.c
+++ arreglos8.c
@@ -6,6 +6,9 @@
 //  @return varible de tipo int
 //  @param una matriz
 //  @param el número de filas
+int suma (int a[][3], int f );
+int suma2 (int **a, int f, int c);
+
 
 
 int main() {
@@ -13,12 +16,35 @@ int main() {
 
     // TODO: (23) Crea una variable llamada resultado  y asigna lo que devuelve la función suma
     // pasa a la función el arreglo y su tamaño
-    
-    
-    // TODO: (24) Imprime el resultado
-    
+    int res = suma (arr, 3); 
+    int *ptr = &res; 
+    printf("%d\t", *(ptr));
+   // res=suma2(&arr,3, 3);              //nno funciona porque 
+    //printf("%d\t", *(ptr));
     return 0;
 }
 
-
+int suma2 (int **a, int f, int c){
+nt suma (int a[][3], int f ){
+    int r = 0;
+    for (int i = 0; i < f; i++) {
+            for(int j=0; j <c; j++){
+            r+=a[i][j];
+            }
+    }
+}
+        }
 //TODO: (25) Crea la función suma que realiza la suma de todos los elementos de un arreglo
+
+int suma (int a[][3], int f ){
+    int r = 0;
+    for (int i = 0; i < f; i++) {
+            for(int j=0; j <3; j++){
+            r+=a[i][j];
+
+        }
+
+    }
+return r;
+
+    }
